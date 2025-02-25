2e492366cfdcc28fd54536f79a84d87494b05c7a
Samuel Peets <samuel@blastworksinc.ca>
2025-02-25 12:25:46 -0800

    add /

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -6,12 +6,12 @@ export default function Header() {
     return (
         <div className="w-full p-2">
             <div className="flex">
-                <Link href="" className="px-2">
+                <Link href="/" className="px-2">
                     <h2>HN PWA</h2>
                 </Link>
-                <Link href="" className="px-2">top</Link>
-                <Link href="best" className="px-2">best</Link>
-                <Link href="new" className="px-2">new</Link>
+                <Link href="/" className="px-2">top</Link>
+                <Link href="/best" className="px-2">best</Link>
+                <Link href="/new" className="px-2">new</Link>
 
             </div>
         </div>
