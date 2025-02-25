9648a8273378eef50c1134f12ba7284e2fef753c
Samuel Peets <samuel@blastworksinc.ca>
2025-02-25 11:48:25 -0800

    maybe

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="HN PWA" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </head>
